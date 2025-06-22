29cadf18ab078d41fdd41900eae6c9d2370e3bc6
Santosh Bhandari <santoshvandari1@gmail.com>
2025-06-22 09:43:45 +0545

    feat: create swaggerUI docs for development

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -40,17 +40,24 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'rest_framework', # Django REST Framework for API development
+    'corsheaders', # CORS headers for cross-origin requests
+
+
+
+    'drf_yasg',  # For API documentation Swagger
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # CORS middleware
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -142,3 +149,10 @@ SIMPLE_JWT = {
     # "SIGNING_KEY": "<Use Strong in Production>",  # ToDo: Use a more secure key in production
 
 }
+
+
+# CORS settings
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",  # React app running on localhost
+# ]
+CORS_ALLOW_ALL_ORIGINS = True  # Allow all origins for development; restrict in production
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,12 +15,41 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from rest_framework_simplejwt import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import permissions
+
+
+# Swagger UI configuration and Documentation
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import path, re_path
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Accounting System API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+
+
+    # Swagger UI and API Documentation
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
 
