e4bde255885eda3d293f86b3e416beb15379dfe8
Santosh Bhandari <santoshvandari1@gmail.com>
2025-06-28 10:01:56 +0545

    feat: model for transaction created

--- backend/transactions/models.py
+++ backend/transactions/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET("Deleted User"),related_name="transactions")
+    received_from = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    note = models.TextField(blank=True, null=True)
+    date = models.DateField(default=timezone.now)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date', '-created_at']
+
+    def __str__(self):
+        return f"Rs.{self.amount} from {self.received_from} by {self.user.full_name}"
--- backend/transactions/urls.py
+++ backend/transactions/urls.py
@@ -3,9 +3,22 @@ from transactions import views
 from rest_framework import status,permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from transactions import views
 
 
 
 urlpatterns = [
     # path("/")
 ]
+
+
+
+# Suggested Endpoints for transactions App
+# Method	Endpoint	Description
+# POST	/api/transactions/	Create a new transaction
+# GET	/api/transactions/	List all transactions (filterable)
+# GET	/api/transactions/<id>/	Get details of a specific transaction
+# PUT	/api/transactions/<id>/	Update a transaction
+# DELETE	/api/transactions/<id>/	Delete a transaction
+# GET	/api/transactions/summary/	Daily/weekly/monthly income/expense summary (optional)
+# GET	/api/transactions/payment-methods/	List available payment methods (dropdown)
