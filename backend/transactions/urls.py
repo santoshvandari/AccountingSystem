078bf6e328b8ec94691679a3963a847498a0c45b
Santosh Bhandari <santoshvandari1@gmail.com>
2025-06-30 08:54:43 +0545

    feat: #4 slove the comment

--- backend/transactions/urls.py
+++ backend/transactions/urls.py
@@ -14,13 +14,3 @@ urlpatterns = [
 ]
 
 
-
-# Suggested Endpoints for transactions App
-# Method	Endpoint	Description
-# POST	/api/transactions/	Create a new transaction
-# GET	/api/transactions/	List all transactions (filterable)
-# GET	/api/transactions/<id>/	Get details of a specific transaction
-# PUT	/api/transactions/<id>/	Update a transaction
-# DELETE	/api/transactions/<id>/	Delete a transaction
-# GET	/api/transactions/summary/	Daily/weekly/monthly income/expense summary (optional)
-# GET	/api/transactions/payment-methods/	List available payment methods (dropdown)
